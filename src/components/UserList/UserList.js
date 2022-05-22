f828bc3a2da8b991628944a07035ee083ea8e1e4
Marian <marianlopig@gmail.com>
2022-05-23 01:22:50 +0200

    changed imagepath to show them

--- src/components/UserList/UserList.js
+++ src/components/UserList/UserList.js
@@ -1,11 +1,23 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { loadUsersThunk } from "../../redux/thunks/usersThunk/usersThunk";
 import UserProfile from "../UserProfile/UserProfile";
 import { UserListStyles } from "./UserListStyles";
 
 const UserList = () => {
+  const users = useSelector(({ users }) => users);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadUsersThunk());
+  }, [dispatch]);
+
   return (
     <UserListStyles>
       <div className="maincontainer">
-        <UserProfile />
+        {users.map((user) => (
+          <UserProfile user={user} />
+        ))}
       </div>
     </UserListStyles>
   );
--- src/redux/thunks/usersThunk/usersThunk.js
+++ src/redux/thunks/usersThunk/usersThunk.js
@@ -5,7 +5,12 @@ const API_URL = "https://two02204-w7chwe-marian-lopez.onrender.com";
 export const loadUsersThunk = () => async (dispatch) => {
   const { data, status } = await axios.get(`${API_URL}/users/list`);
 
+  const users = data.map((user) => ({
+    ...user,
+    image: `${API_URL}/${user.image}`,
+  }));
+
   if (status === 200) {
-    dispatch(loadUsersActionCreator(data));
+    dispatch(loadUsersActionCreator(users));
   }
 };
